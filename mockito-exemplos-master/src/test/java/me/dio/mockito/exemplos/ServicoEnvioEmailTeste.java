75856945cd52e426afb17141c5df2e5aac466b0d
Vinny Moraes <56524332+VinnyMoraes-dev@users.noreply.github.com>
2023-12-02 11:19:19 -0300

    Add files via upload

---
+++ mockito-exemplos-master/src/test/java/me/dio/mockito/exemplos/ServicoEnvioEmailTeste.java
@@ -0,0 +1,61 @@
+package me.dio.mockito.exemplos;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * Teste da classe {@link ServicoEnvioEmail} exemplificando testes usando Argument Captor
+ */
+@ExtendWith(MockitoExtension.class)
+public class ServicoEnvioEmailTeste {
+	
+	@Mock
+	private PlataformaDeEnvio plataforma;
+	
+	@InjectMocks
+	private ServicoEnvioEmail servico;
+	
+	@Captor
+	ArgumentCaptor<Email> emailCaptor;
+	
+	@Test
+	void validaSeEmailEstaComDadosCorretos() {
+		String enderecoDeEmail = "vinny@gmail.com";
+		String mensagem = "Olá mundo teste mensgame";
+//		boolean ehFormatoHtml = false;
+		
+		servico.enviaEmail(enderecoDeEmail, mensagem, true);
+		Mockito.verify(plataforma).enviaEmail(emailCaptor.capture());
+		
+		Email emailCapturado = emailCaptor.getValue();
+		Assertions.assertEquals(Formato.HTML, emailCapturado.getFormato());
+	}
+	
+	
+	
+//    @Mock
+//    private PlataformaDeEnvio plataforma;
+//
+//    @InjectMocks
+//    private ServicoEnvioEmail servico;
+//
+//    @Captor
+//    private ArgumentCaptor<Email> emailCaptor;
+//
+//    @Test
+//    public void validaSeEmailEstaComDadosCorretos() {
+//
+//        String email = "jose.alve@provedor.com";
+//        String mensagem = "Mensagem de Teste 123";
+//
+//        servico.enviaEmail(email, mensagem, true);
+//        Mockito.verify(plataforma).enviaEmail(emailCaptor.capture());
+//
+//        Email emailCapturado = emailCaptor.getValue();
+//        Assertions.assertEquals(Formato.HTML, emailCapturado.getFormato());
+//    }
+
+}
